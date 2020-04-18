80ea52bc5c194ff5d8d3681bf12f58115b0daaaf
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-18 16:52:21 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,8 +8,11 @@ block_return_values = []
     i = i + 1   
   end
 
-if block_return_values.include?(false) false
-else true
+if block_return_values.include?(false) 
+  false
+else 
+  true
 end
 end
+
 my_all?([1,2,3]) {|i| i < 2}
